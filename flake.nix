626e8ce18847eef3cfaa628e7b98c0bd7e2b0cd4
Yipeng Sun <syp@umd.edu>
2022-01-09 03:53:19 +0100

    A basic flake.

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "A pointer to a version of nixpkgs.";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      rec {
+        packages = flake-utils.lib.flattenTree {
+          gitAndTools = pkgs.gitAndTools;
+        };
+        defaultPackage = packages.gitAndTools;
+      }
+    );
+}
